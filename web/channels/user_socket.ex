bcb11e730a35b98df80048860b51f131481309a8
Soshi Katsuta <katsuta_soshi@cyberagent.co.jp>
2015-08-16 22:23:39 +0900

    web: add socket
    Signed-off-by: Soshi Katsuta <katsuta_soshi@cyberagent.co.jp>

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule ElixirSample.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "rooms:*", ElixirSample.RoomChannel
+  channel "rooms:*", ElixirSample.RoomChannel
 
   ## Transports
   transport :websocket, Phoenix.Transports.WebSocket
